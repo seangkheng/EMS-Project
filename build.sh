4c530e1f79cc2fb10b82fa0fee970ed7d08251bb
Seangkheng <seangkheng.hou@gmail.com>
2025-08-12 16:57:12 +0700

    Update build.sh with more font libraries

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
-apt-get update && apt-get install -y libpango-1.0-0 libpangoft2-1.0-0
+# Install system dependencies for fonts and PDF generation
+apt-get update && apt-get install -y \
+  libpango-1.0-0 \
+  libpangoft2-1.0-0 \
+  libcairo2 \
+  libgdk-pixbuf2.0-0 \
+  fonts-noto \
+  fonts-noto-cjk
 
+# Install python dependencies
 pip install -r requirements.txt
